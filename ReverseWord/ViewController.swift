a1963cf7679f3b78d9478ff4c856e86f260b5268
Vitaly Khryapin <vitkhryapin@gmail.com>
2021-11-29 10:50:09 +0300

    func reversWords split two func

--- ReverseWord/ViewController.swift
+++ ReverseWord/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var segmentControllerOutlet: UISegmentedControl!
     @IBOutlet weak var AllCharactersExceptionLabel: UILabel!
     let bottomLine = CALayer()
-   
+    
     
     override func viewDidLoad() {
         super .viewDidLoad()
@@ -49,13 +49,21 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let paragraphStyle = NSMutableParagraphStyle()
         reverseButtonOutlet.titleLabel?.font = UIFont(name: "SFProDisplay-Regular", size: 17)
         paragraphStyle.lineHeightMultiple = 1.08
+        
+        buttonConstraints()
+        
+        
+        
+        
+    }
+    
+    func buttonConstraints () {
         let parentButton = self.view!
         parentButton.addSubview(reverseButtonOutlet)
         reverseButtonOutlet.translatesAutoresizingMaskIntoConstraints = false
         reverseButtonOutlet.heightAnchor.constraint(equalToConstant: 56).isActive = true
         reverseButtonOutlet.leadingAnchor.constraint(equalTo: parentButton.leadingAnchor, constant: 16).isActive = true
         reverseButtonOutlet.trailingAnchor.constraint(equalTo: parentButton.trailingAnchor, constant: -16).isActive = true
-        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -68,44 +76,62 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     func reversWords()  {
+        if segmentControllerOutlet.selectedSegmentIndex == 0 {
+            selectedSegmentDefault()
+        } else {
+            selectedSegmentCustom()
+        }
+    }
+    
+    //inputTF.endEditing(false) // if button not Hidden
+    
+    
+    func selectedSegmentDefault() {
         if let string: String = inputTF.text {
             let words = string.components(separatedBy: " ")
             let character = string.compactMap{$0}
             let reverseString = words.map{String($0.reversed())}
             var numberOfExceptionalValues = 0
             var resultString: String = ""
-            if segmentControllerOutlet.selectedSegmentIndex == 0 {
-                let reverseCharacter = reverseString
-                    .joined(separator: " ")
-                    .filter{$0.isLetter}
-                    .compactMap{$0}
-                for indexCharacter in character.indices {
-                    if !character[indexCharacter].isLetter {
-                        resultString = resultString + String(character[indexCharacter])
-                        numberOfExceptionalValues += 1
-                    }else{
-                        resultString = resultString + String(reverseCharacter[indexCharacter-numberOfExceptionalValues])
-                    }
+            let reverseCharacter = reverseString
+                .joined(separator: " ")
+                .filter{$0.isLetter}
+                .compactMap{$0}
+            for indexCharacter in character.indices {
+                if !character[indexCharacter].isLetter {
+                    resultString = resultString + String(character[indexCharacter])
+                    numberOfExceptionalValues += 1
+                } else {
+                    resultString = resultString + String(reverseCharacter[indexCharacter-numberOfExceptionalValues])
                 }
-            }else{
-                let exceptional: String = textIgnoreTF.text!
-                let characterExceptional = Array(exceptional)
-                let reverseCharacter = reverseString
-                    .joined(separator: " ")
-                    .filter{!characterExceptional.contains($0)}
-                    .compactMap{$0}
-                for indexCharacter in character.indices {
-                    if  characterExceptional.contains(character[indexCharacter]) {
-                        resultString += String(character[indexCharacter])
-                        numberOfExceptionalValues += 1
-                    }else{
-                        resultString += String(reverseCharacter[indexCharacter - numberOfExceptionalValues])
-                    }
-                }
+                resultLabel.text = "\(resultString)"
             }
-            resultLabel.text = "\(resultString)"
         }
-        //inputTF.endEditing(false) // if button not Hidden
+    }
+    
+    func selectedSegmentCustom() {
+        if let string: String = inputTF.text {
+            let words = string.components(separatedBy: " ")
+            let character = string.compactMap{$0}
+            let reverseString = words.map{String($0.reversed())}
+            var numberOfExceptionalValues = 0
+            var resultString: String = ""
+            let exceptional: String = textIgnoreTF.text!
+            let characterExceptional = Array(exceptional)
+            let reverseCharacter = reverseString
+                .joined(separator: " ")
+                .filter{!characterExceptional.contains($0)}
+                .compactMap{$0}
+            for indexCharacter in character.indices {
+                if  characterExceptional.contains(character[indexCharacter]) {
+                    resultString += String(character[indexCharacter])
+                    numberOfExceptionalValues += 1
+                } else {
+                    resultString += String(reverseCharacter[indexCharacter - numberOfExceptionalValues])
+                }
+                resultLabel.text = "\(resultString)"
+            }
+        }
     }
     
     func clearFields() {
@@ -116,7 +142,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func editingBegan(_ sender: UITextField) {
         reverseButtonOutlet.alpha = 1
         bottomLine.backgroundColor = UIColor(red: 0, green: 0.478, blue: 1, alpha: 1).cgColor
-       
+        
     }
     
     @IBAction func editingEnded(_ sender: UITextField) {
@@ -124,7 +150,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func segmentControllerChanged(_ sender:  UISegmentedControl) {
-        
         switch sender.selectedSegmentIndex {
         case 0:
             textIgnoreTF.isHidden = true
@@ -156,15 +181,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
         if  inputTF.text == "" || inputTF.text == nil   {
             
             resultLabel.text = "Please type text"
-        }else if inputTF.text != "" && resultLabel.text != "" && resultLabel.text != "Please type text" {
+        } else if inputTF.text != "" && resultLabel.text != "" && resultLabel.text != "Please type text" {
             clearFields()
-        }else{
+        } else {
             reversWords()
         }
         
         if inputTF.text == "" {
             reverseButtonOutlet.setTitle("Reverse", for: .normal)
-        }else{
+        } else {
             reverseButtonOutlet.setTitle("Clear", for: .normal)
         }
     }
